44b24ee8f3b34602629dd209ca533ebc589115af
Eyezik <eyezikbra@gmail.com>
2022-11-15 10:21:52 -0600

    Update zmap.py
    MADE FOR PYTHON3

--- zmap.py
+++ zmap.py
@@ -4,25 +4,27 @@ import socket, ftplib
 import random
 import pickle ###serialize shit/ or make it kill itself before it gets too big a buffer 
 import time
-print "\n +-+-+-+-+-+-+-+-+-+-+-+"
-print   " |--------ZMAP---------|"
-print   " +-+-+-+-+-+-+-+-+-+-+-+"
-print "use:zmap.py port threads output.txt"
+print ("\n +-+-+-+-+-+-+-+-+-+-+-+")
+print   (" |--------ZMAP---------|")
+print   (" +-+-+-+-+-+-+-+-+-+-+-+")
+print ("use:zmap.py port threads output.txt")
 threads = str(sys.argv[2])
 count = 0
 ipf=open('list.txt', 'w')
 ipf.write('')
 ipf.close()
-running = 0
+
 port = int(sys.argv[1])
 def yadigg():
+    global running
+    running = (0)
 #    for x in range(50): use thisif you on normal hydra not the shitty windows one
-        while running < 10: #lazy mans thread killing by making tog switch/doesnt need on linux but the windows hydra is fucky
+    while running < 10: #lazy mans thread killing by making tog switch/doesnt need on linux but the windows hydra is fucky
             p=random.randint(1,254)
             q=random.randint(1,254)
             r=random.randint(1,254)
             s=random.randint(1,254)
-            global running
+            #global running
             ip=str(p) + "." +str(q) + "." +str(r) + "." +str(s)
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,7 +34,7 @@ def yadigg():
                     #dns, alias, addresslist = socket.gethostbyaddr(ip)
                     #dns.replace("'", '')
                     #print(dns)
-                    print " [X] FOUND: " + ip +" [X] "
+                    print (" [X] FOUND: " + ip +" [X] ")
                     ipf=open('list.txt', 'a')
                     ipf.write(ip +  '\n')
                     ipf.close()
@@ -45,7 +47,7 @@ def yadigg():
                     #print " [!] FOUND: " + ip + " [!]\n"
                     pass
             #count = count + 1 
-            except Exception ,e:
+            except: #Exception ,e:
                 pass         
 for threads in range(0, int(threads)):
 	try:
@@ -56,4 +58,3 @@ for threads in range(0, int(threads)):
 	except:
 		print('Thread failed: ' + str(count))
 print('Threads: ' + str(count))
-
